ee3d148659cee914d30f75a7215e3ff4cfd87df6
HamzaDevelops42 <smarttalk4242@gmail.com>
2025-12-03 19:36:48 +0500

    add username validation

--- src/services/supabase/components/sign-up-form.tsx
+++ src/services/supabase/components/sign-up-form.tsx
@@ -14,8 +14,9 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { SocialLoginForm } from './social_auth_button'
+import { CheckIcon, Loader2, XIcon } from 'lucide-react'
 
 export function SignUpForm({ className, ...props }: React.ComponentPropsWithoutRef<'div'>) {
   const [email, setEmail] = useState('')
@@ -24,6 +25,9 @@ export function SignUpForm({ className, ...props }: React.ComponentPropsWithoutR
   const [username, setUsername] = useState('')
   const [error, setError] = useState<string | null>(null)
   const [isLoading, setIsLoading] = useState(false)
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isUsernameAvailable, setIsUsernameAvailable] = useState<boolean | null>(null)
+
   const router = useRouter()
 
   const handleSignUp = async (e: React.FormEvent) => {
@@ -33,8 +37,13 @@ export function SignUpForm({ className, ...props }: React.ComponentPropsWithoutR
     setError(null)
 
     if (password !== repeatPassword) {
-      setError('Passwords do not match')
+      setError("Passwords don't match")
       setIsLoading(false)
+      return
+    }
+
+    if (!isUsernameAvailable) {
+      setError("Username is already taken")
       return
     }
 
@@ -57,6 +66,31 @@ export function SignUpForm({ className, ...props }: React.ComponentPropsWithoutR
       setIsLoading(false)
     }
   }
+
+  useEffect(() => {
+    if (!username.trim()) {
+      setIsUsernameAvailable(null)
+      return
+    }
+    setIsCheckingUsername(true)
+    setIsUsernameAvailable(null)
+
+    const timeout = setTimeout(async () => {
+      const supabase = createClient()
+
+      const { data } = await supabase
+        .from("profiles")
+        .select("id")
+        .eq("username", username)
+        .maybeSingle()
+
+      setIsUsernameAvailable(!data)
+      setIsCheckingUsername(false)
+    }, 500) //  debounce delay
+
+    return () => clearTimeout(timeout)
+  }, [username])
+
 
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
@@ -87,8 +121,29 @@ export function SignUpForm({ className, ...props }: React.ComponentPropsWithoutR
                   placeholder=""
                   required
                   value={username}
-                  onChange={(e) => setUsername(e.target.value)}
+                  onChange={(e) => setUsername(e.target.value.trim())}
                 />
+                {isCheckingUsername && (
+                  <span className='flex items-center gap-2'>
+                    <Loader2 className="animate-spin size-4 text-muted-foreground" />
+                    <p className="text-xs text-muted-foreground">Checking availability...</p>
+
+                  </span>
+                )}
+
+                {isUsernameAvailable === true && (
+                  <span className='flex items-center gap-2'>
+                    <CheckIcon className='size-4 text-green-600 dark:text-green-400' />
+                    <p className="text-xs text-green-600"><></>Username is available </p>
+                  </span>
+                )}
+
+                {isUsernameAvailable === false && (
+                  <span className='flex items-center gap-2'>
+                    <XIcon className='text-red-400 size-4' />
+                    <p className="text-xs text-red-400">Username is already taken </p>
+                  </span>
+                )}
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
@@ -114,8 +169,8 @@ export function SignUpForm({ className, ...props }: React.ComponentPropsWithoutR
                   onChange={(e) => setRepeatPassword(e.target.value)}
                 />
               </div>
-              {error && <p className="text-sm text-red-500">{error}</p>}
-              <Button type="submit" className="w-full" disabled={isLoading}>
+              {error && <p className="text-xs text-red-400">{error}</p>}
+              <Button type="submit" className="w-full" disabled={isLoading || isUsernameAvailable === false || isCheckingUsername}>
                 {isLoading ? 'Creating an account...' : 'Sign up'}
               </Button>
             </div>
